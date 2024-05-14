5bb1a0934cdf327c617c19c4aceddb287e20bc97
prathmesh988 <prathmeshgawade41@gmail.com>
2024-05-14 23:37:27 +0530

    finalized

--- main.c
+++ main.c
@@ -2,17 +2,16 @@
 
 
 int main(){
-    // my_printf("less go s%dasdf &&&%% %f", 10, 10.567);
-    
+   
     my_printf("Large number: %f\n", 0.6);
     my_printf("Large number: %f\n", 0.123456);
-    //print the %0.2f with float value 0.67
+
     my_printf("Large number: %0.2f\n", 2.123434);
 
     my_printf("Large number: %0.2f\n", 0.123456);
     my_printf("integer number %d %0.2f %2345 %0.2f obama \n", 10,5.3223,  10.567);
 
-    // print the long  valu134e and long int value 
+   
     long int num = 1234234890;
     my_printf("long value %ld",num);
     return 0;
